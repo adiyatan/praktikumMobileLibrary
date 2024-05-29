04246e8cbcbb524084ae94ad3f1ff5bb2bbaa1f9
adiyatan <adiyazam03@gmail.com>
2024-05-29 22:18:44 +0700

    chore: Update navigation screen routes and add book dialog

--- app/src/main/java/id/ac/unpas/agenda/persistences/AppDatabase.kt
+++ app/src/main/java/id/ac/unpas/agenda/persistences/AppDatabase.kt
@@ -1,6 +1,8 @@
 package id.ac.unpas.agenda.persistences
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import id.ac.unpas.agenda.models.Book
 import id.ac.unpas.agenda.models.Member
@@ -13,6 +15,14 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun bookRequestDao(): BookRequestDao
 
     companion object {
+        private var INSTANCE: AppDatabase? = null
         const val DATABASE_NAME = "baruuuuuu"
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                INSTANCE = Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME).build()
+            }
+            return INSTANCE!!
+        }
     }
 }
